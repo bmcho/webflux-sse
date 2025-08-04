865a137f7eb5445b9a9d46ee5da8040fc6f57c78
bmcho <bmcho91@naver.com>
2025-08-04 16:50:54 +0900

    add: 간단한 webflux 기반 sse

---
+++ src/test/java/com/bmcho/webfluxsse/WebfluxSseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmcho.webfluxsse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxSseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
